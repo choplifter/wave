5d24f3abb3dda07655d0b25d1b85bd389f668e5e
Choplifter <a.kindlbacher@gmx.de>
2025-05-13 22:21:55 +0200

    Socialite vendor files added to git

---
+++ vendor/laravel/socialite/src/Two/TeslaProvider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Laravel\Socialite\Two;
+
+use Illuminate\Support\Arr;
+
+class TeslaProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The base Tesla API URL.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'https://auth.tesla.com/oauth2/v3';
+
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['openid', 'email', 'offline_access'];
+
+    /**
+     * Get the authentication URL for Tesla.
+     *
+     * @param  string  $state
+     * @return string
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase("{$this->baseUrl}/authorize", $state);
+    }
+
+    /**
+     * Get the token URL for Tesla.
+     *
+     * @return string
+     */
+    protected function getTokenUrl()
+    {
+        return "{$this->baseUrl}/token";
+    }
+
+    /**
+     * Get the user by token.
+     *
+     * @param  string  $token
+     * @return array
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get("{$this->baseUrl}/userinfo", [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Map the raw user array to a Socialite User instance.
+     *
+     * @param  array  $user
+     * @return \Laravel\Socialite\Two\User
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['sub'],
+            'nickname' => null,
+            'name' => $user['name'] ?? null,
+            'email' => $user['email'] ?? null,
+            'avatar' => $user['picture'] ?? null,
+        ]);
+    }
+}
