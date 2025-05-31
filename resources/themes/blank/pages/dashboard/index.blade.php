e050e04b1304f39585cb07d9cc3cfb08192b9c94
Andreas Kindlbacher <a.kindlbacher@gmail.com>
2025-05-31 13:04:44 +0200

    dashboard format

--- resources/themes/blank/pages/dashboard/index.blade.php
+++ resources/themes/blank/pages/dashboard/index.blade.php
@@ -9,7 +9,7 @@ name('dashboard');
     <div class="max-w-5xl lg:px-4 mx-auto">
         <h2 class="text-2xl font-bold">Welcome, {{ auth()->user()->name }} 👋</h2>
     </div>
-    <div class="max-w-5xl lg:px-6 mx-auto mt-8 space-y-8">
+    <div class="grid grid-cols-1 gap-10 md:grid-cols-2  lg:px-6 lg:py-6 mt-8 space-y-8 space-x-6">
         <div class="flex md:space-x-4 md:space-y-0 space-y-6 md:flex-row flex-col">
             <x-app.dashboard-card icon="phosphor-user" label="Users" value="143" change-direction="up"
                 change-percentage="25"></x-app.dashboard-card>
@@ -25,9 +25,7 @@ name('dashboard');
                     Volt</a> page. Learn more about <a href="https://devdojo.com/wave/docs/pages" target="_blank"
                     class="underline">how pages work</a> here.</p>
         </div>
-        <div class="w-full p-10 space-y-4 border border-black">
-            @livewire('counter')
-        </div>
+
         <div class="w-full p-10 space-y-4 border border-black">
             @livewire('teslatoken')
         </div>
