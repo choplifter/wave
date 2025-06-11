19db0f531c49459231515228cc5196b24c29f805
Choplifter <a.kindlbacher@gmx.de>
2025-06-11 13:53:42 +0200

    domain fix

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Wave\Facades\Wave;
 use Laravel\Socialite\Facades\Socialite;
 
 
-Route::domain('api.ilogistix.net')->group(function () {
+Route::domain('www.ilogistix.net')->group(function () {
     Route::middleware(['auth', 'web'])->group(function () {
         // Forward all /api/tesla/* calls to the local Tesla vehicle HTTP proxy using Teslacore token
         Route::any('/api/1/{any}', [\App\Http\Controllers\TeslaApiProxyController::class, 'forward'])
